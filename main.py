f713d65a10be13e5af13f5eed152bd65dea8b3c8
michal7899 <71443812+michal7899@users.noreply.github.com>
2020-10-14 19:38:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import bs4, sys
+
+
+driver = webdriver.Chrome()
+site = input('Which website would you like to use, reddit or instagram?')
+
+if site == 'reddit':
+    driver.get('https://www.reddit.com/')
+elif site == 'instagram':
+    raise Exception('instagram use has not yet been implemented')        # User-generated error message
+    #driver.quit()
+    # sys.exit()
+else:
+    #print('Wrong website has been entered')
+    raise Exception('Wrong website has been entered')         # User-generated error message
+
+
+searchElem = driver.find_element_by_id('header-search-bar')
+
